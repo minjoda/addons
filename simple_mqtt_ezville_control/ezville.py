89239987d396a256d258b1a72d838f89cfec6603
minjoda <167152899+minjoda@users.noreply.github.com>
2024-06-29 11:23:00 +0900

    Update ezville.py

--- simple_mqtt_ezville_control/ezville.py
+++ simple_mqtt_ezville_control/ezville.py
@@ -979,7 +979,7 @@ def ezville_loop(config):
     from paho.mqtt.enums import CallbackAPIVersion
     mqtt_client = mqtt.Client(CallbackAPIVersion.VERSION1, 'mqtt-ezville')
     mqtt_client.username_pw_set(config['mqtt_id'], config['mqtt_password'])
-    mqtt_client.on_connect on_connect
+    mqtt_client.on_connect = on_connect
     mqtt_client.on_disconnect = on_disconnect
     mqtt_client.connect_async(config['mqtt_server'])
     
